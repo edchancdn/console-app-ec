7fbef27f1cd2e70158ea32a9091366f876349fd4
edchancdn <edchan.cdn@gmail.com>
2022-10-03 23:04:46 -0400

    Added menu options. Updated runner to input data line by line. Added validations. Added unit tests

--- src/main/java/co/pragra/learning/Runner.java
+++ src/main/java/co/pragra/learning/Runner.java
@@ -2,34 +2,112 @@ package co.pragra.learning;
 
 import co.pragra.learning.business.ContactService;
 import co.pragra.learning.model.Contact;
+import co.pragra.learning.model.ContactType;
+import org.apache.commons.lang3.StringUtils;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.util.ServiceConfigurationError;
 
 public class Runner {
+
     public static void main(String[] args) {
-        Scanner scanner  = new Scanner(System.in);
-        System.out.println("Key in Data separated by space");
-
-        String next = scanner.nextLine();
-        String[] split = next.split(" ");
-        Contact contact = Contact.builder().firstName(split[0])
-                .lastName(split[1])
-                .phoneNumber(split[2])
-                .build();
-
+        boolean leaveApp = false;
+        String firstName, lastName, phoneNumber, contactType = "";
         ContactService service = new ContactService();
+        Scanner scanner  = new Scanner(System.in);
 
-        service.addContact(contact);
-        System.out.println("Key in Data separated by space");
-        String next1 = scanner.nextLine();
-        String[] split1 = next1.split(" ");
-        Contact contact1 = Contact.builder().firstName(split1[0])
-                .lastName(split1[1])
-                .phoneNumber(split1[2])
-                .build();
-        service.addContact(contact1);
+        System.out.println("Menu options:");
+        System.out.println("l - List all contacts");
+        System.out.println("a - Add a contact");
+        System.out.println("e - Edit a contact");
+        System.out.println("d - Delete a contact");
+        System.out.println("x - Exit the application");
 
-        service.printAll();
+        do {
+            System.out.println("");
+            System.out.print("Enter an option: ");
 
+            String option = scanner.nextLine();
+
+            switch (option) {
+                case "l":
+                    System.out.println("CONTACT LIST:");
+                    service.printAll();
+                    break;
+                case "e":
+                    try {
+                        System.out.println("EDIT CONTACT...");
+                        System.out.print("Enter the phone number of the contact to edit: ");
+                        phoneNumber = scanner.nextLine();
+                        Contact c = service.getContactByPhone(phoneNumber);
+                        System.out.print("Enter the first name (" + c.getFirstName() + ") : ");
+                        firstName = scanner.nextLine();
+                        System.out.print("Enter the last name (" + c.getLastName() + ") : ");
+                        lastName = scanner.nextLine();
+                        System.out.print("Enter the contact type (" + c.getContactType() + ") : ");
+                        contactType = scanner.nextLine();
+                        if (ContactType.findByName(String.valueOf(contactType)) == null) {
+                            throw new RuntimeException("Invalid contact type value");
+                        }
+                        Contact contact = Contact.builder().firstName(firstName)
+                                .lastName(lastName)
+                                .phoneNumber(phoneNumber)
+                                .contactType(ContactType.valueOf(contactType))
+                                .build();
+                        service.addContact(contact);
+                        System.out.println("CONTACT EDITED.");
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                        System.out.println("CONTACT NOT EDITED.");
+                    }
+                    break;
+                case "a":
+                    try {
+                        System.out.println("ADD CONTACT...");
+                        System.out.print("Enter the phone number : ");
+                        phoneNumber = scanner.nextLine();
+                        if (service.checkIfExist(phoneNumber) < 0) {
+                            System.out.print("Enter the first name: ");
+                            firstName = scanner.nextLine();
+                            System.out.print("Enter the last name: ");
+                            lastName = scanner.nextLine();
+                            Contact contact = Contact.builder().firstName(firstName)
+                                    .lastName(lastName)
+                                    .phoneNumber(phoneNumber)
+                                    .build();
+                            service.addContact(contact);
+                            System.out.println("CONTACT ADDED.");
+                        } else {
+                            System.out.println("Contact already exist");
+                            System.out.println("CONTACT NOT ADDED.");
+                        }
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                        System.out.println("CONTACT NOT ADDED.");
+                    }
+                    break;
+                case "d":
+                    try {
+                        System.out.println("DELETE CONTACT...");
+                        System.out.print("Enter the phone number of the contact to delete: ");
+                        phoneNumber = scanner.nextLine();
+                        service.deleteContact(phoneNumber);
+                        System.out.println("CONTACT DELETED.");
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                        System.out.println("CONTACT NOT DELETED.");
+                    }
+                    break;
+                case "x":
+                    leaveApp = true;
+                    System.out.println("Exiting the application...");
+                    break;
+                default:
+                    System.out.println("Please choose a valid menu option...");
+                    break;
+            }
+        }
+        while (!leaveApp);
     }
 }
